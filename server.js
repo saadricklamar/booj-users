3f75b5b8f41a36eb70d8798750aab6d31eacb83b
Saad Baradan <saadbaradan@gmail.com>
2019-12-04 14:38:49 -0700

    Add get method for fetching all users

--- server.js
+++ server.js
@@ -6,3 +6,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}.`);
+});
+
+app.get("/api/v1/users", (request, response) => {
+  database("users")
+    .select()
+    .then(users => {
+      response.status(200).json(users);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
