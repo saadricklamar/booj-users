80fecfccc6fc72953a4b8bb7fef3c19ff1a6b1a3
Saad Baradan <saadbaradan@gmail.com>
2019-12-06 12:26:56 -0700

    Refactor Get all users using try and catch

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const configuration = require("./knexfile")[environment];
 const database = require("knex")(configuration);
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
+
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
-
+app.use(bodyParser.json());
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}.`);
 });
 
-app.get("/api/v1/users", (request, response) => {
-  database("users")
-    .select()
-    .then(users => {
-      response.status(200).json(users);
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
+app.get("/api/v1/users", async (request, response) => {
+  try {
+    const users = await database("users").select();
+    response.status(200).json(users);
+  } catch (error) {
+    response.status(500).json({ error });
+  }
 });
 
 app.get("/api/v1/users/:id", (request, response) => {
@@ -77,3 +77,5 @@ app.delete("/api/v1/users/:id", (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+module.exports = app;
