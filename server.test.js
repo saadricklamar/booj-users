21df16a320b637324fd4d47065804a094fbfda37
Saad Baradan <saadbaradan@gmail.com>
2019-12-06 12:00:36 -0700

    Install supertest and configure knexfile with test

--- server.test.js
+++ server.test.js
@@ -3,3 +3,10 @@ const configuration = require("./knexfile")[environment];
 const database = require("knex")(configuration);
 const request = require("supertest");
 const app = require("./server.js");
+
+describe("server", () => {
+  beforeEach(async () => {
+    await database.seed.run();
+  });
+  describe("GET /api/v1/users", () => {});
+});
